ec035a5a1a1a4a3c7a195d73227262c73aa51606
steamfsouth <steamfsouth@gmail.com>
2017-12-03 16:16:50 -0500

    Update login.php

--- login.php
+++ login.php
@@ -83,6 +83,7 @@ if(isset($_POST["createAccount"])) {
         connectAndQuery($query);
         $query = "INSERT INTO groupid_groupname (groupid, groupname) values ('$groupId', '$groupName');";
         connectAndQuery($query);
+        $_SESSION['groupId'] = $groupId;
         $_SESSION['email'] = $email;
         header("Location: createAccount.php");
     } else {

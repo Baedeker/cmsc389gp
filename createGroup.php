6d20c23126742773fb8151f88c55dae7636697dc
vmelandri12 <virginiamelandri@gmail.com>
2017-12-03 13:23:05 -0500

    flexbox

--- createGroup.php
+++ createGroup.php
@@ -1,12 +1,11 @@
 <?php
-    require_once 'support.php';
-    session_start();
+require_once 'support.php';
+session_start();
 
-    echo "<link rel=\"stylesheet\" href=\"flexbox.css\">";
+echo "<link rel=\"stylesheet\" href=\"flexbox.css\">";
 
-    class createGroup {
-
-        function groupIdExists($id)
+class createGroup {
+    function groupIdExists($id)
     {
         $query = "SELECT * FROM email_group WHERE groupid = '$id'";
         $result = connectAndQuery($query);
@@ -16,7 +15,6 @@
             return false;
         }
     }
-
     function userExists($email)
     {
         $query = "SELECT * FROM users WHERE email = '$email'";
@@ -27,36 +25,30 @@
             return false;
         }
     }
-
 }
 if(isset($_POST["create"])) {
-
     do{
         $s = substr(str_shuffle(str_repeat("abcdefghijklmnopqrstuvwxyz", 5)), 0, 4);
-	    $groupId = $s.(rand(1,1000));
-        } while (createGroup::groupIdExists($groupId));
-
-
-        $email = $_POST["email"];
-        $password = $_POST["password"];
-        $groupName = $_POST['groupName'];
-        $firstName = $_POST['firstName'];
-        $lastName = $_POST['lastName'];
-
+        $groupId = $s.(rand(1,1000));
+    } while (createGroup::groupIdExists($groupId));
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $groupName = $_POST['groupName'];
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-	    if(!createGroup::userExists($email)) {
-            $query = "INSERT INTO users (email, password, groupid, firstname, lastname) values ('$email', '$hashedPassword', '$groupId', '$firstName', '$lastName');";
-            connectAndQuery($query);
-            $query = "INSERT INTO email_group (email, groupid, groupname) values ('$email', '$groupId', '$groupName');";
-            connectAndQuery($query);
-        }else{
-	        $_SESSION['firstName'] = $firstName;
-	        $_SESSION['lastName'] = $lastName;
-	        $_SESSION['groupName'] = $groupName;
-            echo "<script type='text/javascript'>alert(\"Username already exists\");window.location=\"createGroup.php\";</script>";
-        }
-
-        $body = <<<BODY
+    if(!createGroup::userExists($email)) {
+        $query = "INSERT INTO users (email, password, groupid, firstname, lastname) values ('$email', '$hashedPassword', '$groupId', '$firstName', '$lastName');";
+        connectAndQuery($query);
+        $query = "INSERT INTO email_group (email, groupid, groupname) values ('$email', '$groupId', '$groupName');";
+        connectAndQuery($query);
+    }else{
+        $_SESSION['firstName'] = $firstName;
+        $_SESSION['lastName'] = $lastName;
+        $_SESSION['groupName'] = $groupName;
+        echo "<script type='text/javascript'>alert(\"Username already exists\");window.location=\"createGroup.php\";</script>";
+    }
+    $body = <<<BODY
         <div class="flex-container">
         
         <div class="container-fluid bg-3 text-center header">
@@ -76,35 +68,20 @@ if(isset($_POST["create"])) {
 </div>
 BODY;
     generatePage($body, 'Sign Up');
-    } else{
+} else{
     $firstName = (isset($_SESSION['firstName']))? $_SESSION['firstName'] : "";
     $lastName = (isset($_SESSION['lastName']))? $_SESSION['lastName'] : "";
     $groupName = (isset($_SESSION['groupName']))? $_SESSION['groupName'] : "";
     unset($_SESSION['firstName']);
     unset($_SESSION['lastName']);
     unset($_SESSION['groupName']);
-        $body = <<<BODY
-<<<<<<< HEAD
-        <div class="flex-container">
-    <form action="createGroup.php" method="POST">
-    <div class="container-fluid bg-4 text-center">
-    <h1>First, create a personal account:</h1>
-    </div>
-    <div class="container-fluid bg-3 text-center">
-        <strong>First Name </strong><input type="text" name="firstName" required value=$firstName><br><br>
-        <strong>Last Name </strong><input type="text" name="lastName" required value=$lastName><br><br>
-        <strong>Group Name </strong><input type="text" name="groupName" required value=$groupName><br><br>
-        <strong>Email </strong><input type="email" name="email" required/><br><br>
-        <strong>Create Password </strong><input type="password" name="password" required/><br><br>
-        <input type="submit" name="create" value="OK"/><br><br>
-=======
+    $body = <<<BODY
         
-        <div class="flexcontainer">
+        <div class="flex-container">
         <form action="{$_SERVER['PHP_SELF']}" method="POST">
             <div class="container-fluid bg-4 text-center">
                 <h1>First, create a personal account:</h1>
             </div>
-
             <div class="container-fluid bg-3 text-center">
                 <strong>First Name </strong><input type="text" name="firstName" required value=$firstName><br><br>
                 <strong>Last Name </strong><input type="text" name="lastName" required value=$lastName><br><br>
@@ -112,13 +89,9 @@ BODY;
                 <strong>Email </strong><input type="email" name="email" required/><br><br>
                 <strong>Create Password </strong><input type="password" name="password" required/><br><br>
                 <input type="submit" name="create" value="OK"/><br><br>
->>>>>>> cebeed5bd8601b5a2e9b070a0a7eb0af7ff4d349
         </div>
-
     </form>
     </div>
-
 BODY;
     generatePage($body, 'Sign Up');
-    }
-
+}

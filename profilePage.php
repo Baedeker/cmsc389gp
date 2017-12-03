59b6e3cf5506987530653ca187683ebaed45aaaa
Alex Li <angalexli@gmail.com>
2017-12-03 18:59:48 -0500

    Update profilePage.php
    Added functionality to update progress bars

--- profilePage.php
+++ profilePage.php
@@ -145,10 +145,75 @@ if (isset($_POST['submit'])) {
     $result = connectAndQuery($sql);
     if ($result) {
         $right .= "Successfully added!<br>";
-        $right .= "Please refresh the page to see any new updates that you've submitted.<br><br>";
+        //$right .= "Please refresh the page to see any new updates that you've submitted.<br><br>";
     } else {
         $right .= "Submission failed.<br><br>";
     }
+    
+    $query = "SELECT * FROM Progress WHERE email='$profileemail' && type='general'"; // pulling general percentage
+    $result = connectAndQuery($query);
+    $temp = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $currentGeneralPercent = $temp['percentage'];
+
+    $query = "SELECT * FROM Progress WHERE email='$profileemail' && type='sleep'"; // pulling sleep percentage
+    $result = connectAndQuery($query);
+    $temp = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $currentSleepPercent = $temp['percentage'];
+
+    $query = "SELECT * FROM Goals WHERE email='$profileemail'"; // pulling goals
+    $temp = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $bedtimeGoal = $temp['bedtimeGoal'];
+    $fallAsleepGoal = $temp['fallAsleepGoal'];
+    $troubleAwakeGoal = $temp['troubleAwakeGoal'];
+
+    $sleepprogressmod = 0;
+    $diff = calculateMinuteDifference($bedtimeGoal, $timeib);
+    if ($diff > 120) {
+        $sleepprogressmod += 0;
+    } else if ($diff > 60) {
+        $sleepprogressmod += 0.03;
+    } else {
+        $sleepprogressmod += 0.06;
+    }
+    $diff = 60*($timefallsleephours - $timeinbedhours) + ($timefallasleepminutes - $timeinbedminutes);
+    if ($diff > 60) {
+
+    } else if ($diff > 30) {
+        $sleepprogress += 0.02;
+    } else {
+        $sleepprogress += 0.04;
+    }
+    $generalprogressmod = 0.10;
+    $currentGeneralPercent += $generalprogressmod;
+    $currentSleepPercent += $sleepprogressmod;
+
+    if ($currentGeneralPercent > 1) { // updating stars if general percent is over 100%
+        $currentGeneralPercent -= 1;
+        $query = "SELECT * FROM users WHERE email='$profileemail'";
+        connectAndQuery($query);
+        $temp = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        $stars = $temp['stars'];
+        $stars++;
+        $query = "UPDATE users SET stars = '$stars' WHERE email='$profileemail'";
+        connectAndQuery($query);
+    }
+    if ($currentSleepPercent > 1) { // updating stars if sleep percent is over 100%
+        $currentSleepPercent -= 1;
+        $query = "SELECT * FROM users WHERE email='$profileemail'";
+        connectAndQuery($query);
+        $temp = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        $stars = $temp['stars'];
+        $stars++;
+        $query = "UPDATE users SET stars = '$stars' WHERE email='$profileemail'";
+        connectAndQuery($query);
+    }
+
+    $query = "UPDATE Progress SET percentage = '$currentGeneralPercent' WHERE email='$profileemail' && type='general'"; // pushing general percentage
+    $result = connectAndQuery($query);
+
+    $query = "UPDATE Progress SET percentage = '$currentSleepPercent' WHERE email='$profileemail' && type='sleep'"; // pushing sleep percentage
+    $result = connectAndQuery($query);	
+    
     $topandleft = generateTable($profilename, $profileemail, $currentuseremail, $left);
     unset($_POST['submit']);
 }

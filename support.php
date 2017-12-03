c6dd4ac55c32c921b6c9e76c16265d7b2c323e3a
jonathan <jrmevs@umd.edu>
2017-12-03 14:02:14 -0500

    js validation, change to support

--- support.php
+++ support.php
@@ -8,8 +8,8 @@ function generatePage($body, $title, $script="") {
         <meta charset="utf-8" />
         <title>$title</title> 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-        <link rel="stylesheet" href="main.css">
         <link rel ="shortcut icon" href="favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" href="main.css">
     </head>
             
     <body>
@@ -19,7 +19,6 @@ function generatePage($body, $title, $script="") {
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="$script"></script>
 </html>
 EOPAGE;
@@ -39,10 +38,11 @@ function connectAndQuery($query){
     $db_connection->close();
 }
 function generateRadioSelect($name){
-    $toReturn = <<<RET
+    if($name != "overall"){
+        $toReturn = <<<RET
         <div class="form-check form-check-inline">
                     <label class="form-check-label">
-                        <input class="form-check-input" type="radio" name="$name"  checked="checked" value="Not during the past month">
+                        <input class="form-check-input" type="radio" name="$name"  value="Not during the past month">
                         Not during the past month
                     </label>
                 </div>
@@ -68,6 +68,39 @@ function generateRadioSelect($name){
                 </div>
 RET;
     return $toReturn;
+    }
+    else{
+        $toReturn = <<<RET
+        <div class="form-check form-check-inline">
+                    <label class="form-check-label">
+                        <input class="form-check-input" type="radio" name="$name"  checked="checked" value="Not during the past month">
+                        Very Good
+                    </label>
+                </div>
+               <div class="form-check form-check-inline">
+                    <label class="form-check-label">
+                        <input class="form-check-input" type="radio" name="$name"  value="Less than once a week">
+                        Fairly Good
+                    </label>
+                </div>                     
+                
+                <div class="form-check form-check-inline">
+                    <label class="form-check-label">
+                        <input class="form-check-input" type="radio" name="$name"  value="Once or twice a week">
+                        Fairly bad
+                    </label>
+                </div>
+                
+                <div class="form-check form-check-inline">
+                    <label class="form-check-label">
+                        <input class="form-check-input" type="radio" name="$name"  value="Three or more times a week">
+                        Very bad
+                    </label>
+                </div>
+RET;
+    return $toReturn;
+    }
+    
 }
 function generateResults($bedtime, $fallAsleep, $wakeup, $actualSleep, $timeInBed, $within30, $middleOfNight, $troubleAwake, $overall){
     $toReturn = <<<RET

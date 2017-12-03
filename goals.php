ba4998a0f3947c52961da4079a76bae36f852c7a
steamfsouth <steamfsouth@gmail.com>
2017-12-03 15:50:08 -0500

    Update goals.php

--- goals.php
+++ goals.php
@@ -8,13 +8,22 @@
     $query = "INSERT INTO Goals values ('$email','$bedtimeGoal','$fallAsleepGoal','$troubleAwakeGoal')";
     connectAndQuery($query);
 
+    $query = "SELECT firstname,lastname ".
+            "FROM users ".
+            "WHERE email='$email'";
+    $result = connectAndQuery($query);
+    $recordArray = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $currentuser = $recordArray['firstname']." ".$recordArray['lastname'];
+
+
+
     $body = <<<BODY
         <div class="container">
             <div class="row">
                 <h3>Awesome, your goals are all set! Let's head over to your customized
                 profile page</h3>
 
-                <form action="profilePage.php" method="post">
+                <form action="profilePage.php?profilename=$currentuser" method="post">
                     <input type="hidden" name="email" value="$email">
                     <button type="submit" name="fromGoals" class="btn btn-primary">To Profile Page</button>
                 </form>

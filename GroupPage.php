89f16d7f5f69fca1091bf468bc76278c9fa5feba
jonathan <jrmevs@umd.edu>
2017-12-03 14:53:37 -0500

    link js file

--- GroupPage.php
+++ GroupPage.php
@@ -22,6 +22,7 @@
     $result = connectAndQuery($query);
     $recordArray = mysqli_fetch_array($result, MYSQLI_ASSOC);
     $groupname = $recordArray['groupname'];
+    echo $recordArray[0];
 
     $top = <<<BODY
         <div class="container-fluid bg-2">
@@ -186,59 +187,5 @@ BODY;
 BODY;
 
     $body = $top.$left.$right;
-    generatePage($body, 'Group Page');
+    generatePage($body, 'Group Page', 'groupPage.js');
 ?>
-<script>
-    function logout() {
-        request = $.ajax({
-            url: "ajax_logout.php",
-            type: "post"
-        });
-        window.location.href="main.html";
-    }
-
-    function clickSendGroup(currentuser,groupid) {
-        let message = document.getElementById("message").value;
-        if (message != "") {
-            let datetime = getCurrentTime();
-            $.ajax({
-                url: "ajax_insert.php",
-                type: "post",
-                data: {query : "INSERT INTO `Message` (`message`, `sender`, `recipient`, `datetime`, `groupid`)"
-                     + "VALUES ('"
-                     + message + "','"
-                     + currentuser + "','group','"
-                     + datetime + "','"
-                     + groupid + "')"}
-            });
-            document.getElementById("groupchat").innerHTML =
-                currentuser+" <small>"+datetime+"</small><br/>"
-                +"<small>&emsp;"+message+"</small><br/>"+document.getElementById("groupchat").innerHTML;
-            document.getElementById("message").value = "";
-        }
-    }
-
-    function clickSendPersonal(name,currentuser,groupid) {
-        let message = document.getElementById("message"+name).value;
-        if (message != "") {
-            let datetime = getCurrentTime();
-            document.getElementById("personalmessage").value +=
-                "INSERT INTO `Message` (`message`, `sender`, `recipient`, `datetime`, `groupid`)"
-                + "VALUES ('"
-                + message + "','"
-                + currentuser + "','"
-                + name + "','"
-                + datetime + "','"
-                + groupid + "')";
-        }
-    }
-
-    function getCurrentTime() {
-        let currentdate = new Date();
-        return currentdate.getFullYear() + "-"
-           +  (currentdate.getMonth()+1) + "-"
-           +  currentdate.getDate() + " " +
-           + currentdate.getHours() + ":"
-           + currentdate.getMinutes();
-    }
-</script>
